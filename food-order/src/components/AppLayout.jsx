32bfd4cc35ed43469ba971e8f8ea5b47b2d33b56
Jeckygabut26 <Jecky.gusmendra47@smk.belajar.id>
2026-08-26 10:40:30 +0700

    update

--- food-order/src/components/AppLayout.jsx
+++ food-order/src/components/AppLayout.jsx
@@ -1,4 +1,4 @@
-import useTheme from "../hooks/useTheme";
+import useTheme from "./hooks/useTheme";
 
 const AppLayout = ({ title, children, actions }) => {
   const { theme, toggleTheme, isDark } = useTheme();
--- food-order/src/components/hooks/useTheme.js
+++ food-order/src/components/hooks/useTheme.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../provider/ThemeContext";
+import { ThemeContext } from "../../provider/ThemeContext";
 
 export const useTheme = () => {
   const context = useContext(ThemeContext);
